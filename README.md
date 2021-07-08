94c2538dc29452937de225e9e861e0a64870688c
Craig <indigomx9@gmail.com>
2021-07-08 12:35:20 -0400

    Initial setup of the Project.

---
+++ README.md
@@ -0,0 +1,6 @@
+# React Testing
+
+Testing a React app with Jest, and React Testing Library.
+
+
+
---
+++ src/app/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.StrictMode>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint aperiam laborum libero repellat amet. Molestias similique sequi eaque consequuntur rerum nobis magni. Corrupti consequatur magni voluptatem tempora quos, voluptate accusantium.</p>
+        </React.StrictMode>
+    );
+};
+
+
+
+
